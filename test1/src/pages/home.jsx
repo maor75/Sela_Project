0c5b89f5aea3385f4bb06b81cb446573d72d67c0
Maor Avidan <129369594+maor75@users.noreply.github.com>
2024-06-17 14:55:32 +0300

    Update home.jsx

--- test1/src/pages/home.jsx
+++ test1/src/pages/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>Welcome To Home Page</h1>
+            <h1>Welcome To Home Page!!!</h1>
             <h2>Crowned Company</h2>
             <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <img src="/Untitled_Artwork.ico" alt="Favicon" style={{ width: '250px', height: '300px' }} />
